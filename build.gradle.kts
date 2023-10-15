33f8e494ae6e9a169e3c4fa399d6d589ef285a8a
Helltar <iam@helltar.com>
2023-10-16 00:28:00 +0300

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,10 @@ repositories {
 
 dependencies {
     implementation ("com.annimon:tgbots-module:6.5.1")
-    implementation("ch.qos.logback:logback-classic:1.4.6")
+    implementation("ch.qos.logback:logback-classic:1.4.7")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
-    implementation("org.jsoup:jsoup:1.15.3")
+    implementation("org.jsoup:jsoup:1.15.4")
     implementation("org.slf4j:slf4j-api:2.0.6")
 }
 
