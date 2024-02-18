3b9e5299d05bfd3ae890f5b02dd75351f23c0f38
Helltar <iam@helltar.com>
2024-02-18 18:04:59 +0200

    upgrade tgbots-module version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ repositories {
 }
 
 dependencies {
-    implementation ("com.annimon:tgbots-module:6.5.1")
-    implementation("ch.qos.logback:logback-classic:1.4.7")
+    implementation ("com.annimon:tgbots-module:7.1.0")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation("org.jsoup:jsoup:1.15.4")
