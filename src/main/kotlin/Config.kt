ab4afe7c8b6789b64d1115763c145deac8b61d5c
Helltar <iam@helltar.com>
2024-11-02 14:48:48 +0200

    add config object to manage environment variables

---
+++ src/main/kotlin/Config.kt
@@ -0,0 +1,15 @@
+import io.github.cdimascio.dotenv.dotenv
+
+object Config {
+
+    val creatorId =
+        readEnvVar("CREATOR_ID").toLongOrNull()
+            ?: throw IllegalArgumentException("Invalid value for CREATOR_ID")
+
+    val telegramBotToken = readEnvVar("BOT_TOKEN")
+    val telegramBotUsername = readEnvVar("BOT_USERNAME")
+
+    private fun readEnvVar(env: String) =
+        dotenv { ignoreIfMissing = true }[env].ifBlank { throw IllegalArgumentException("$env environment variable is blank") }
+            ?: throw IllegalArgumentException("Error reading $env environment variable")
+}
