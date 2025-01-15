b790fc7065fe26b00b4adb5be85e9aa3acb17e80
Helltar <iam@helltar.com>
2025-01-16 01:03:02 +0200

    update kotlin, tgbots-module, and project version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,25 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.1.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     application
 }
 
 group = "com.helltar"
-version = "1.2.0"
+version = "1.3.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("com.annimon:tgbots-module:7.10.0") {
+    implementation("com.annimon:tgbots-module:8.0.0") {
         exclude("org.telegram", "telegrambots-webhook")
     }
 
-    implementation("io.github.cdimascio:dotenv-kotlin:6.4.2")
     implementation("ch.qos.logback:logback-classic:1.4.14")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.2")
     implementation("org.jsoup:jsoup:1.15.4")
     implementation("org.slf4j:slf4j-api:2.0.6")
 }
