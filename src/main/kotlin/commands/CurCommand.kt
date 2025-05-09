21081348e1dc6f40496aad954f3e7a7f59e112e6
Helltar <iam@helltar.com>
2025-05-09 21:03:34 +0300

    replace slf4j with kotlin-logging and improve error handling and user messages in CurCommand

--- src/main/kotlin/commands/CurCommand.kt
+++ src/main/kotlin/commands/CurCommand.kt
@@ -9,7 +9,7 @@ import com.annimon.tgbotsmodule.commands.CommandRegistry
 import com.annimon.tgbotsmodule.commands.SimpleCommand
 import com.annimon.tgbotsmodule.commands.authority.For
 import com.annimon.tgbotsmodule.commands.context.MessageContext
-import org.slf4j.LoggerFactory
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.telegram.telegrambots.meta.api.methods.ParseMode
 
 class CurCommand : CommandBundle<For> {
@@ -17,42 +17,40 @@ class CurCommand : CommandBundle<For> {
     private companion object {
         const val COMMAND = "/cur"
         const val DEFAULT_LANGUAGE_CODE = "en"
+        val log = KotlinLogging.logger {}
     }
-
-    private val log = LoggerFactory.getLogger(javaClass)
 
     override fun register(registry: CommandRegistry<For>) {
         registry.register(SimpleCommand(COMMAND) { ctx ->
             if (ctx.arguments().isEmpty()) {
-                replyToMessage(ctx, "ℹ\uFE0F <code>$COMMAND 1 usd to uah</code>") // ℹ️
+                replyToMessage(ctx, """ℹ️ How to use: <code>$COMMAND</code> <u>1 usd to uah</u>""")
                 return@SimpleCommand
             }
 
-            val query =
-                ctx.argumentsAsString().apply {
-                    if (length !in 8..64) {
-                        replyToMessage(ctx, "✋ 8..64 characters")
-                        return@SimpleCommand
-                    }
-                }
+            val query = ctx.argumentsAsString()
+
+            if (query.length !in 8..64) {
+                replyToMessage(ctx, """⚠️ Please enter a query between 8 and 64 characters""")
+                return@SimpleCommand
+            }
 
             val languageCode = ctx.message().from.languageCode ?: DEFAULT_LANGUAGE_CODE
 
             try {
                 val googleHtml = fetchGoogleHtml(query, languageCode)
+                var result = extractCurrencyInfo(googleHtml) ?: extractCoinInfo(googleHtml)
 
-                val result =
-                    extractCurrencyInfo(googleHtml) ?: extractCoinInfo(googleHtml) ?: run {
-                        log.error("Invalid google response: $googleHtml")
-                        "\uD83E\uDEE3 Error parsing Google response" // 🫣
-                    }
+                if (result == null) {
+                    result = """❌ Error when parsing google response"""
+                    log.error { "Invalid google response: $googleHtml" }
+                }
 
                 val googleFinanceLink = extractGoogleFinanceLink(googleHtml)
 
                 replyToMessage(ctx, "$result\n\n$googleFinanceLink")
             } catch (e: Exception) {
-                log.error(e.message)
-                replyToMessage(ctx, "${e.message}")
+                log.error { e.message }
+                replyToMessage(ctx, "❌ ${e.message}")
             }
         })
     }
