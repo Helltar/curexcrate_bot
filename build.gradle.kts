f3345b1a697a0c9a011bbbf02aecefae2b5af258
Helltar <iam@helltar.com>
2025-05-09 21:03:23 +0300

    update dependencies and plugins in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    kotlin("jvm") version "2.1.20"
+    id("com.gradleup.shadow") version "8.3.6"
     application
 }
 
 group = "com.helltar"
-version = "1.5.0"
+version = "1.6.0"
 
 repositories {
     mavenCentral()
@@ -19,7 +19,8 @@ dependencies {
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.2")
     implementation("org.jsoup:jsoup:1.15.4")
-    implementation("ch.qos.logback:logback-classic:1.5.16")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.7")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
 }
 
 application {
